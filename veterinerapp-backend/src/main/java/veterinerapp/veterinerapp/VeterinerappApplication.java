82fea37088d3b94b66c9e31ed0ec019882712cc4
ruskyvisky <numan_859@hotmail.com>
2023-08-27 00:36:42 +0300

    first commit

---
+++ veterinerapp-backend/src/main/java/veterinerapp/veterinerapp/VeterinerappApplication.java
@@ -0,0 +1,13 @@
+package veterinerapp.veterinerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinerappApplication.class, args);
+	}
+
+}
---
+++ veterinerapp-backend/src/test/java/veterinerapp/veterinerapp/VeterinerappApplicationTests.java
@@ -0,0 +1,13 @@
+package veterinerapp.veterinerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
