de2032e0d818ae17815486e38945110067ee7722
ruskyvisky <numan_859@hotmail.com>
2023-09-23 04:37:57 +0300

    refactor [login-form] changed login form structure

--- veterinerap-frontend/src/app/(auth)/login/page.tsx
+++ veterinerap-frontend/src/app/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
-import LoginPage from '@/containers/auth'
 import React from 'react'
+import LoginPage from '@/containers/auth/'
 
 const page = () => {
   return(
--- veterinerap-frontend/src/containers/auth/index.tsx
+++ veterinerap-frontend/src/containers/auth/index.tsx
@@ -1,58 +1,64 @@
 "use client"
-import React from 'react';
-import { Form, Input, Button, Row, Col, Card } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import React from 'react'
+import { Form, Input, Button, Typography, Card, Row, Col } from 'antd';
+import  {loginType}  from '@/types/authTypes'
 
-const LoginPage = () => {
-
-
-  return (
-    <Row justify="center" align="middle" style={{ minHeight: '100vh' }}>
-      <Col span={8}>
-        <Card title="Giriş Yap" bordered={true}>
-          <Form
-            name="login_form"
-           
-          >
-            <Form.Item
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: 'E-posta adresinizi girin!',
-                },
-              ]}
-            >
-              <Input
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="E-posta"
-              />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Şifrenizi girin!',
-                },
-              ]}
-            >
-              <Input.Password
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Şifre"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
-                Giriş Yap
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
-      </Col>
-    </Row>
-  );
+const {Link } = Typography;
+const onFinish = (values: loginType) => {
+  console.log('Success:', values);
 };
 
-export default LoginPage;
+const onFinishFailed = (errorInfo: any) => {
+  console.log('Failed:', errorInfo);
+};
+
+const LoginPage = () => {
+  return (
+    <Row justify="center" align="middle" style={{ minHeight: '100vh' }}>
+    <Col span={8}>
+      <Card title="Giriş Yap" bordered={true}>
+<Form
+  name="basic"
+  initialValues={{ remember: true }}
+  onFinish={onFinish}
+  onFinishFailed={onFinishFailed}
+  autoComplete="off"
+>
+  <Form.Item<loginType>
+    name="email"
+    rules={[{ required: true, message: 'Lütfen E-Postanızı girin!' }]}
+
+  >
+    <Input placeholder='E-Posta' />
+  </Form.Item>
+
+  <Form.Item<loginType>
+    
+    name="password"
+    rules={[{ required: true, message: 'Lütfen Şifrenizi Girin!' }]}
+  >
+    <Input.Password placeholder='Şifre'  />
+  </Form.Item>
+
+
+
+  <Form.Item style={{display:'flex' , justifyContent:'center'}}>
+    <Button type="primary" htmlType="submit">
+      Giriş Yap
+    </Button>
+  </Form.Item>
+</Form>
+<div style={{display:'flex' , justifyContent:'center'}}>
+<Link href="/test" >
+      Hesabın Yok mu? Kayıt ol
+    </Link>
+</div>
+        </Card>
+        </Col>
+      </Row>
+
+
+  )
+}
+
+export default LoginPage
